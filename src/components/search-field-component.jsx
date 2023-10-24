793fcd56770c7f3fb63967da08a0a904c8aeb0aa
Akhil Sharma <akhilsharma6721@gmail.com>
2023-10-24 16:44:00 +0000

    Update search-field-component.jsx

--- src/components/search-field-component.jsx
+++ src/components/search-field-component.jsx
@@ -12,7 +12,7 @@ const SearchField = () => {
   useEffect(() => {
     if (debouncedSearch)
       fetchImages(
-        `/search/photos?page=1&query=${debouncedSearch}&client_id=${process.env.REACT_APP_ACCESS_KEY}`
+        `/search/photos?page=1&query=${debouncedSearch}&client_id=${process.env.REACT_APP_ACCESS_KEY}`,
       );
   }, [debouncedSearch]);
 
@@ -21,7 +21,7 @@ const SearchField = () => {
   };
 
   return (
-    <div className="flex justify-center items-center text-xs md:text-sm bg-[#f6f6f6] border-2 py-1 md:px-4 md:py-2 gap-2 text-gray-600 border-gray-200 rounded-md">
+    <div className="flex justify-center items-center text-xs md:text-sm bg-[#f6f6f6] border-2 px-2 py-1 md:px-4 md:py-2 gap-2 text-gray-600 border-gray-200 rounded-md">
       <HiMagnifyingGlass size={20} />
       <input
         value={query}
